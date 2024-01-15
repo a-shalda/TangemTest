d0dff24b6fd5ac01acb516e1c1274521581130b2
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2024-01-15 14:31:08 +0400

    добавил local storage

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const bannerOne = document.querySelector('.banner--one')
 const bannerTwo = document.querySelector('.banner--two')
 
+// localStorage.clear(); 
+
+if (JSON.parse(localStorage.getItem('bannerVisible')) === false) {
+  bannerTwo.style.display = 'none';
+}
+
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   const html = document.documentElement;
@@ -18,17 +24,15 @@ document.addEventListener("scroll", () => {
     bannerTwo.style.visibility = 'visible';
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     bannerTwo.style.opacity = scrollTop / 100 - 1;
-
-    
-    if (window.innerWidth < 600) {
-      bannerTwo.style.width = 100;
-    }
-    else if (window.innerWidth >= 600) {
-      bannerTwo.style.width = 600;
-    }
   }
   else if (isInViewport(bannerOne)) {
     bannerTwo.style.visibility = 'hidden';
   }
 })
 
+const close = document.querySelector('.banner--two__right__button--close')
+
+close.addEventListener("click", () => {
+  bannerTwo.style.display = 'none';
+  localStorage.setItem('bannerVisible', JSON.stringify(false));
+})
